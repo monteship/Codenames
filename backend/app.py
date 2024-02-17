168cc64a02843ea20c5e3866f4fb324606b0f06b
monteship <monteship@gmail.com>
2024-02-17 20:12:33 +0200

    looks fine

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 import secrets
+from functools import wraps
 
 import flask
 from flask import Flask
@@ -23,8 +24,8 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = secrets.token_hex(24)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///codenames.sqlite"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-CORS(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
+CORS(app)
 
 db = SQLAlchemy(app)
 
@@ -35,40 +36,68 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     role = db.Column(db.String(20), nullable=False)
-    color = db.Column(db.String(20), nullable=False)
+    color = db.Column(db.String(20), nullable=False, default="grey")
     token = db.Column(db.String(255), nullable=False, unique=True)
-
-    def __repr__(self):
-        return {"username": self.username, "color": self.color, "role": self.role}
 
     @classmethod
     def create_user(cls, username, role):
         token = secrets.token_hex(24)
-        user = cls(username=username, token=token, role=role, color="neutral")
-        db.session.add(user)
+        db_user = cls(username=username, token=token, role=role, color="grey")
+        db.session.add(db_user)
         db.session.commit()
-        return user
+        return db_user
 
     @classmethod
     def find_by_token(cls, token):
-        return cls.query.filter_by(token=token).first()
+        user = cls.query.filter_by(token=token).first()
+        return user
+
+    @classmethod
+    def players(cls):
+        users = cls.query.all()
+        members = {
+            "red": {"players": [], "spymaster": ["Spymaster"]},
+            "blue": {"players": [], "spymaster": ["Spymaster"]},
+            "grey": {"players": []},
+        }
+        for user in users:
+            if not user.role == "all":
+                members[user.color][user.role].append(user.username)
+        return members
+
+    def update_role(self, new_role):
+        self.role = new_role
+        db.session.commit()
+
+    def update_color(self, new_color):
+        self.color = new_color
+        db.session.commit()
+
+    def update_username(self, new_name):
+        self.username = new_name
+        db.session.commit()
 
 
 with app.app_context():
     db.create_all()
 
+    for _user in User.query.all():
+        _user.update_role("all")
+        _user.update_color("grey")
 
-def authentication_required(role=None):
+
+def authentication_required(role=None, auth=False):
     def decorator(func):
-        def wrapper(*args, **kwargs):
+        @wraps(func)
+        def decorated_function(*args, **kwargs):
             token = flask.request.headers.get("Authorization").split(" ")[-1]
             user = User.find_by_token(token)
             if not user:
-                user = User.create_user(playground.get_nickname(), "guest")
-                emit("grantToken", {"token": user.token, "name": user.username})
+                user = User.create_user(playground.get_nickname(), "players")
                 logger.info("New user %s connected", user.username)
-                join_room("guests")
-            logger.info("User %s connected", user.username)
+                emit("auth", {"token": user.token, "username": user.username})
+            if auth:
+                emit("auth", {"token": user.token, "username": user.username})
 
             if role and user.role not in role:
                 logger.warning(
@@ -76,63 +105,83 @@ def authentication_required(role=None):
                     user.username,
                 )
                 return
-
-            emit("updateGameData", playground.codenames)
             if user.role == "spymaster":
+                emit("playersUpdate", User.players(), to="spymaster")
                 emit("spymasterGameData", playground.codenames, to="spymaster")
             kwargs["user"] = user
             return func(*args, **kwargs)
 
-        return wrapper
+        return decorated_function
 
     return decorator
 
 
-@socketio.on("connect")
-@authentication_required
-def handle_connect():
-    join_room("guests")
+@socketio.on("connect", namespace="/")
+@authentication_required(role=None)
+def handle_connect(user: User, auth=True):
+    join_room("all")
+    emit("playersUpdate", User.players(), to="all")
+    emit("updateGameData", playground.codenames, to="all")
 
 
-@socketio.on("restartGame")
+@socketio.on("disconnect", namespace="/")
+@authentication_required(role=None)
+def handle_disconnected(user: User):
+    pass
+
+
+@socketio.on("changeUsername", namespace="/")
+@authentication_required(role=None)
+def handle_change_username(new_username: str, user: User):
+    user.update_username(new_username)
+
+
+@socketio.on("restartGame", namespace="/")
 @authentication_required(role=["players", "spymaster"])
-def handle_restart():
+def handle_restart(user: User):
+    for user in User.query.all():
+        user.update_role("all")
+        user.update_color("grey")
     logger.info("Client requested game restart")
     playground.restart_game()
-    emit("restartedGameData", playground.codenames)
+    emit("restartedGameData", playground.codenames, to="all")
 
 
-@socketio.on("clickAction")
+@socketio.on("restarted", namespace="/")
+@authentication_required(role=None)
+def restart(user: User):
+    leave_room("players")
+    leave_room("spymaster")
+    join_room("all")
+
+
+@socketio.on("clickAction", namespace="/")
 @authentication_required(role=["players"])
-def handle_word_click(word):
+def handle_word_click(word, user: User):
     logger.info("Client clicked word")
     color = playground.trigger_click(word)
-    emit("clickedAction", {"word": word, "color": color})
+    emit("clickedAction", {"word": word, "color": color}, to="all")
 
 
-@socketio.on("becomePlayer")
-@authentication_required(role=["players", "guest"])
-def become_player(color, **kwargs):
-    user: User = kwargs["user"]
-    leave_room("guest")
+@socketio.on("joinPlayers", namespace="/")
+@authentication_required(role=["players", "all"])
+def become_player(color, user: User):
     join_room("players")
-    user.role = "players"
-    user.color = color
-    db.session.commit()
-    emit("newPlayer", kwargs["user"])
+    user.update_role("players")
+    user.update_color(color)
+    emit("playersUpdate", User.players(), to="all")
 
 
-@socketio.on("becomeSpymaster")
+@socketio.on("joinSpymaster", namespace="/")
 @authentication_required(role=["players"])
-def become_spymaster(**kwargs):
-    user: User = kwargs["user"]
-    user.role = "spymaster"
-    db.session.commit()
+def become_spymaster(user: User):
+    user.update_role("spymaster")
     leave_room("players")
     join_room("spymaster")
-    emit("newSpymaster", kwargs["user"])
+    emit("playersUpdate", User.players(), to="all")
+    emit("spymasterGameData", playground.codenames, to="spymaster")
 
 
-@socketio.on("endGame")
+@socketio.on("endGame", namespace="/")
 def handle_endgame():
     emit("gameEnded")
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {io, Socket} from 'socket.io-client';
 import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 interface Score {
   initial_score_red: number;
@@ -14,11 +15,6 @@ interface Score {
   players_blue: [];
 }
 
-interface Member {
-  username: string;
-  color: string;
-  role: string;
-}
 
 interface GameData {
   score: Score;
@@ -26,36 +22,36 @@ interface GameData {
   spymasters: any[];
 }
 
-interface ClickAction {
-  word: string;
-  color: string;
-}
 
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [CommonModule, HttpClientModule]
+  imports: [CommonModule, HttpClientModule,
+    FormsModule]
 })
 
 export class AppComponent implements OnInit {
   ws: Socket;
   gameData: any[] = [];
-  name: string | undefined;
-  spymasterPlaceHolder: string = "Become spymaster"
+  name: string | null = localStorage.getItem('username');
+  redSpymaster: string = "Spymaster";
+  blueSpymaster: string = "Spymaster";
 
   redTeam: string = 'red';
+  blueTeam: string = 'blue';
+
   redScore: number = 0;
   redScoreInitial: number = 0;
   redPlayers: string[] = [];
-  redSpymaster: string = "";
 
-  blueTeam: string = 'blue';
   blueScore: number = 0;
   blueScoreInitial: number = 0;
   bluePlayers: string[] = [];
-  blueSpymaster: string = "";
+
+  redSpymasterButtonDisabled: boolean = false;
+  blueSpymasterButtonDisabled: boolean = false;
 
   constructor() {
     this.ws = io("http://192.168.1.229:5000", {
@@ -70,9 +66,12 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.ws.on("grantToken", (data: { token: string, name: string }) => {
-      localStorage.setItem('token', data["token"])
-      this.name = data["name"];
+    this.ws.on("auth", (data: { "token": string, "username": string }) => {
+      console.log("Received grantToken event:", data);
+
+      localStorage.setItem('token', data.token)
+      localStorage.setItem('username', data.username)
+      this.name = data.username;
     });
     this.ws.on("updateGameData", (data: GameData) => {
       this.mapGameData(data)
@@ -80,17 +79,14 @@ export class AppComponent implements OnInit {
     this.ws.on("restartedGameData", (data: GameData) => {
       this.handleRestartedGameData(data)
     });
-    this.ws.on("clickedAction", (data: ClickAction) => {
-      this.handleClickedAction(data)
+    this.ws.on("clickedAction", (event) => {
+      this.handleClickedAction(event)
     });
-    this.ws.on("newPlayer", (data: Member) => {
-      this.handleNewPlayer(data)
+    this.ws.on("playersUpdate", (event) => {
+      this.handlePlayersUpdate(event)
     });
-    this.ws.on("newSpymaster", (data: Member) => {
-      this.handleNewSpymaster(data)
-    });
-    this.ws.on("spymasterVision", (data: GameData) => {
-      this.handleSpymasterVision(data)
+    this.ws.on("spymasterGameData", (data: GameData) => {
+      this.handleSpymasterGameData(data)
     });
   }
 
@@ -98,13 +94,9 @@ export class AppComponent implements OnInit {
   updateThisData(score: Score) {
     this.redScoreInitial = score.initial_score_red;
     this.redScore = score.score_red;
-    this.redSpymaster = score.spymaster_red;
-    this.redPlayers = score.players_red;
 
     this.blueScoreInitial = score.initial_score_blue;
     this.blueScore = score.score_blue;
-    this.blueSpymaster = score.spymaster_blue;
-    this.bluePlayers = score.players_blue;
   }
 
   private mapGameData(data: GameData) {
@@ -125,22 +117,29 @@ export class AppComponent implements OnInit {
     this.mapGameData(data);
 
     this.redScore = 0;
-    this.redSpymaster = this.spymasterPlaceHolder;
-    this.blueSpymaster = this.spymasterPlaceHolder;
     this.blueScore = 0;
+
+    this.redSpymasterButtonDisabled = false;
+    this.blueSpymasterButtonDisabled = false;
 
     const activeElements = document.querySelectorAll('.active');
     activeElements.forEach(element => {
       element.classList.remove('active', 'blue', 'red', 'yellow', 'black');
+    });
+    const tdElements = document.querySelectorAll('');
+
+    tdElements.forEach(element => {
+      element.classList.remove('blue', 'red', 'yellow', 'black');
     });
 
     const word = document.querySelector(`word`);
     if (word) {
       word.classList.remove('semi');
     }
+    this.ws.emit('restarted');
   }
 
-  handleClickedAction(click: ClickAction) {
+  handleClickedAction(click: { "word": string, "color": string }) {
     const element = this.gameData.find(item => item.word === click.word);
 
     if (click.color === this.redTeam) {
@@ -164,49 +163,36 @@ export class AppComponent implements OnInit {
     }
   }
 
-  handleSpymasterVision(data: GameData) {
+  handleSpymasterGameData(data: GameData) {
     console.log("Received event:", data);
 
     this.updateThisData(data.score);
 
     if (Array.isArray(data.codenames)) {
-      this.gameData = data.codenames.map((wordObject: any) => ({
-        word: wordObject.word,
-        color: !wordObject.clicked ? "semi " + wordObject.color : wordObject.color,
-        clicked: wordObject.clicked
-      }));
-    }
-    const word = document.querySelector(`word`);
-    if (word) {
-      word.classList.add('semi');
+      data.codenames.forEach((wordObject: any) => {
+        const colorClass = wordObject.color;
+        const tdElement = document.querySelector(`td[class="${wordObject.word}"]`);
+        if (tdElement) {
+          tdElement.classList.add(colorClass);
+        }
+      });
     }
   }
 
-
-  handleNewSpymaster(member: Member) {
-    if (member.color === 'red') {
-      const index = this.redPlayers.indexOf(member.username);
-      if (index !== -1) {
-        this.redPlayers.splice(index, 1);
-      }
-      this.redSpymaster = member.username;
-    } else {
-      const index = this.bluePlayers.indexOf(member.username);
-      if (index !== -1) {
-        this.redPlayers.splice(index, 1);
-      }
-      this.blueSpymaster = member.username;
-    }
-
-    this.toggleSpymasterButton(member.color, 'true');
+  updateName() {
+    this.ws.emit("changeUsername", this.name)
   }
 
-  handleNewPlayer(member: Member) {
-    if (member.color === 'red') {
-      this.redPlayers.push(member.username);
-    } else {
-      this.bluePlayers.push(member.username);
-    }
+  handlePlayersUpdate(event: {
+    "red": { "players": string[], "spymaster": string[] },
+    "blue": { "players": string[], "spymaster": string[] },
+    "grey": { "players": string[] }
+  }) {
+    this.redPlayers = event.red.players;
+    this.bluePlayers = event.blue.players;
+
+    this.redSpymaster = event.red.spymaster.join();
+    this.blueSpymaster = event.blue.spymaster.join();
   }
 
   toggleSpymasterButton(team: string, state: string) {
@@ -228,12 +214,12 @@ export class AppComponent implements OnInit {
     }
   }
 
-  becomeSpymaster(event: any, team: string) {
-    this.ws.emit('becomeSpymaster', team);
+  becomeSpymaster(event: any) {
+    this.ws.emit('joinSpymaster');
   }
 
   becomePlayer(event: any, team: string) {
-    this.ws.emit('becomePlayer', team);
+    this.ws.emit('joinPlayers', team);
   }
 
 
